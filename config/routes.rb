ebbb44554f4691814bfa1c0c292a824e4e7ad45a
katyf <katy.feng.14@gmail.com>
2015-04-07 13:33:45 -0400

    changed routes for links, notes, settings to be nested

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  resources :links, except: [:new, :edit]
-  resources :notes, except: [:new, :edit]
-  resources :settings, except: [:new, :edit]
+  # resources :links, except: [:new, :edit]
+  # resources :notes, except: [:new, :edit]
+  # resources :settings, except: [:new, :edit]
 
-  resources :users, except: [:new, :edit]
+  resources :users, except: [:new, :edit] do
+    resources :links, except: [:new, :edit]
+    resources :notes, except: [:new, :edit]
+    resources :settings, except: [:new, :edit]
+  end
+
   post '/login', to: 'users#login'
   get '/logout', to: 'users#logout'
 end
